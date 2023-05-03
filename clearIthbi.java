914cc16a6d84102d7b78eb212ad9658cf1a1550b
Hitesh Saini <hitesh9694saini@gmail.com>
2023-05-04 00:06:29 +0530

    first commit

---
+++ clearIthbi.java
@@ -0,0 +1,9 @@
+public class clearIthbi {
+    public static void main(String[] args) {
+
+        // it is clear 1  it is called clear ith bit 
+        int a =10;
+        int b = 1;
+        System.out.println(a&~(1<<b));
+    }
+}
