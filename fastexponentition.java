620974ffcb7650ca9f8fb48374859db65819013a
Hitesh Saini <hitesh9694saini@gmail.com>
2023-05-04 10:17:53 +0530

    first comit

---
+++ fastexponentition.java
@@ -0,0 +1,15 @@
+public class fastexponentition {
+    public static void main(String[] args) {
+      int ans =1;
+      int a = 3;
+      int n = 5;
+      while(n>0){
+        if((n&1)!=0){
+            ans = ans*a;
+        }
+        a = a*a;
+       n= n>>1;
+      }System.out.println(" ans : "+ans);
+        
+    } 
+}
