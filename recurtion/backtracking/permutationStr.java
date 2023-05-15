96ab5d6d6f684218242d27cbd841cbbf944825f9
Hitesh Saini <hitesh9694saini@gmail.com>
2023-05-15 23:14:20 +0530

    permutation

---
+++ recurtion/backtracking/permutationStr.java
@@ -0,0 +1,29 @@
+package recurtion.backtracking;
+
+public class permutationStr {
+    public static void main(String[] args) {
+        String str = "ab";
+        findPermutation(str, "");
+    }
+    public static void findPermutation(String st , String news ) {
+        // base case
+        if(st.length() == 0){
+           
+            System.out.println(news );
+           
+            return;
+        }
+
+        // recursion
+       for(int i =0;i < st.length();i++){
+        char curr = st.charAt(i);
+        // abcde = ab+bc
+        String newstr = st.substring( 0,i)+st.substring(i+1);
+        findPermutation(newstr, news+curr);
+
+       }
+
+
+
+    }
+}
